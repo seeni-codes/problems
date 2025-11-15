91f08c44410da291164050ecf81b4d327e1618da
seeni-codes <seenivasan.m0174@gamil.com>
2025-11-15 20:21:27 +0530

    updated thing

--- src/Linear.java
+++ src/Linear.java
@@ -3,11 +3,21 @@
 public class Linear {
 
     public static void main(String []args){
-        int [] a={2,35,3,2,234,4};
+        int [] arr={2,35,3,2,234,4};
 
+        int tar=9;
+        boolean is=false;
 
-        for (int i=0;i<a.length;i++){
+        for (int i=0;i<arr.length;i++){
+            if(tar==arr[i]){
+                System.out.println(tar+" target found "+arr[i]);
+                is=true;
+                break;
+            }
+        }
 
+        if(!is){
+            System.out.println("element not found");
         }
     }
 
